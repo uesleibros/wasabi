af45c45b7839dbd48f044c15471e8f22a023ce9b
Ueslei Paim <uesleibros@gmail.com>
2026-05-06 03:28:48 -0300

    Add ws_mask_x86.asm for masking operation

---
+++ dev/asm/ws_mask_x86.asm
@@ -0,0 +1,30 @@
+[bits 32]
+
+start:
+    push ebp
+    mov ebp, esp
+    push edi
+    push esi
+    push ebx
+    mov esi, [ebp + 8]
+    mov ecx, [ebp + 12]
+    mov edi, [ebp + 16]
+    test ecx, ecx
+    jz end
+    xor edx, edx
+loop:
+    mov al, [esi]
+    mov bl, [edi + edx]
+    xor al, bl
+    mov [esi], al
+    inc esi
+    inc edx
+    and edx, 3
+    loop loop
+end:
+    pop ebx
+    pop esi
+    pop edi
+    mov esp, ebp
+    pop ebp
+    ret
