920992f232c850c7cff30fb3a2493adccebf7206
Ueslei Paim <uesleibros@gmail.com>
2026-05-06 03:43:40 -0300

    Refactor memory zeroing routine with masking

--- dev/asm/mem_zero_x86.asm
+++ dev/asm/mem_zero_x86.asm
@@ -1,14 +1,31 @@
 [bits 32]
 
 start:
-    mov ecx, [esp + 8]
-    mov eax, [esp + 4]
+    push ebp
+    mov ebp, esp
+    push edi
+    push esi
+    push ebx
+    mov esi, [ebp + 8]
+    mov ecx, [ebp + 12]
+    mov edi, [ebp + 16]
     test ecx, ecx
     jz end
-loop:
-    mov byte [eax], 0
-    inc eax
+    xor edx, edx
+mask_loop:
+    mov al, [esi]
+    mov bl, [edi + edx]
+    xor al, bl
+    mov [esi], al
+    inc esi
+    inc edx
+    and edx, 3
     dec ecx
-    jnz loop
+    jnz mask_loop
 end:
-    ret
+    pop ebx
+    pop esi
+    pop edi
+    mov esp, ebp
+    pop ebp
+    ret 12
